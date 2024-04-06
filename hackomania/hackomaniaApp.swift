02da9ed077d67cb126d8909e06aea485a04a8de0
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-04-06 11:24:06 +0800

    Initial Commit

---
+++ hackomania/hackomaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackomaniaApp.swift
+//  hackomania
+//
+//  Created by LL on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct hackomaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
