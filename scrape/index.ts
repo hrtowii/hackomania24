59c08c2b0008935f1ba2ab85c44aac33ca7028ee
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-04-07 02:23:01 +0800

    im bored

--- scrape/index.ts
+++ scrape/index.ts
@@ -1,6 +1,8 @@
 import playwright from "playwright"
 import { Database } from './storing/db.ts'
-const browser = await playwright.chromium.launch()
+import { Readability } from "@mozilla/readability"
+import { JSDOM } from "jsdom"
+const browser = await playwright.chromium.launch({headless: false})
 const context = await browser.newContext()
 const page = await context.newPage()
 context.setDefaultTimeout(120000)
@@ -16,30 +18,36 @@ const db = new Database(
 )
 
 const scrape = async (url: string) => {
-    if (url == "") {
-        // continue
-    }
     await page.goto(url)
     for (const li of await page.getByRole('link').all()) { // 
         if (await li.textContent() == "Find Out More") {
             const href = await li.getAttribute('href')
             if (href) {
-                await page.goto(href)
-                let text = await page.locator('body').textContent()
-                console.log(href, text)
+                // check if href has been visited before
+                if (db.exists(href)) {
+                    // console.log(href + " already visited")
+                    continue;
+                }
+                try {
+                    await page.goto(href)
+                } catch (error) {
+                    console.log(error);
+                    continue;
+                }
+                let doc = new JSDOM(await page.content())
+                let reader = new Readability(doc.window.document)
+                let text = reader.parse()!.textContent
+                console.log(text[0])
                 db.push(href, text || "")
+                await page.goto(url);
+                // console.log(href + " done")
                 // scrape(await li.getAttribute('href') || "")
             }
         }
     }
 }
 for (const url of Object.values(urls)) {
-    await scrape(url)
+    await scrape(url).finally(() => { db.close() })
 }
-db.close();
-// await scrape(urls.grants).finally(() => { db.close() })
-// const links = await page.locator.('')
-
-// console.log(links);
 
 await browser.close();
